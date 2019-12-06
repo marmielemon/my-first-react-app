8012b75a217a8c2027216005201610eb46d14934
marmielemon <50599564+marmielemon@users.noreply.github.com>
2019-12-06 09:55:27 -0500

    added profile img and name

---
+++ src/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Profile = () => (
+  <div>
+    <img src="https://avatars3.githubusercontent.com/u/50599564?s=40&v=4" alt="profile pic" />
+    <h1>Marmie Lemon</h1>
+  </div>
+);
+
+export default Profile;
