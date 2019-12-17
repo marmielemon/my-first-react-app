bf24d6e918b6090c4fa9e260d6867a832b352881
marmielemon <50599564+marmielemon@users.noreply.github.com>
2019-12-17 10:54:56 -0500

    Update Profile.js

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -7,6 +7,7 @@ const Profile = ({ person }) => (
     <div className="profile-header">
       <img className="profile-pic" src={person.imgPath} alt="profile pic" />
       <h1 className="profile-heading">{person.firstName} {person.lastName}</h1>
+      <p><a href={`https://github.com/${person.userName}`}>{`@${person.userName}`}</a></p>
     </div>
     <ol className="profile-repositories">
 {
