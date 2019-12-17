0a9223077d1abaf53320cd6ab6031b2b67c7c5c8
marmielemon <50599564+marmielemon@users.noreply.github.com>
2019-12-17 02:56:44 -0500

    completed the css portion

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Profile.css';
 
 const Profile = ({ person }) => (
   <div className="profile-wrapper">
@@ -7,13 +8,13 @@ const Profile = ({ person }) => (
       <img className="profile-pic" src={person.imgPath} alt="profile pic" />
       <h1 className="profile-heading">{person.firstName} {person.lastName}</h1>
     </div>
-    <ul className="profile-repositories">
+    <ol className="profile-repositories">
 {
     person.repositories.map((repo) => (
   <li key={repo.url}><a className="profile-repo-link" href={repo.url}>{repo.name}</a></li>
  ))
   }
-    </ul>
+    </ol>
   </div>
 );
 
